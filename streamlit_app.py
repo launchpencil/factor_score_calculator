94276c1410b1768c9968c1a8a63eddeaaf7766b7
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-12-20 04:24:31 +0000

    修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,55 +35,52 @@ st.write("")
 # 何件法の入力
 n_point_scale = st.number_input("何件法を使用していますか？", value=4, min_value=3, step=1)
 
-# 因子得点の計算とダウンロードボタンの表示
-if uploaded_file_scale_info and uploaded_file_data:
+# 因子得点の計算
+def calculate_factor_scores():
     # ファイルの読み込み
     scale_info = pd.read_excel(uploaded_file_scale_info)
     data = pd.read_excel(uploaded_file_data)
 
-    # ファイル内容の表示
-    st.subheader("＜尺度情報プレビュー＞")
-    st.dataframe(scale_info)
+    # 反転項目の処理
+    for index, row in scale_info.iterrows():
+        if row['反転'] == 1:
+            data[row['設問名']] = n_point_scale - data[row['設問名']]
 
-    st.subheader("＜データプレビュー＞")
-    st.dataframe(data)
+    # 因子得点の算出
+    for factor in scale_info['因子名'].unique():
+        relevant_questions = scale_info[scale_info['因子名'] == factor]['設問名']
+        data[factor + ' 因子得点'] = data[relevant_questions].mean(axis=1)
 
-    # 因子得点を計算するボタン
-    if st.button("因子得点を計算"):
-        # 反転項目の処理
-        for index, row in scale_info.iterrows():
-            if row['反転'] == 1:
-                data[row['設問名']] = n_point_scale - data[row['設問名']]
+    # 不要な列の削除
+    data.drop(columns=scale_info['設問名'], inplace=True)
 
-        # 因子得点の算出
-        for factor in scale_info['因子名'].unique():
-            relevant_questions = scale_info[scale_info['因子名'] == factor]['設問名']
-            data[factor + ' 因子得点'] = data[relevant_questions].mean(axis=1)
+    # セッション状態にデータを保存
+    st.session_state.processed_data = data
 
-        # 不要な列の削除
-        data.drop(columns=scale_info['設問名'], inplace=True)
+# 因子得点を計算するボタン
+if uploaded_file_scale_info and uploaded_file_data:
+    if st.button("因子得点を計算", on_click=calculate_factor_scores):
+        st.session_state.factor_scores_calculated = True
 
-        # 成功メッセージ
-        st.success("因子得点の計算が完了しました。")
+# 成功メッセージと更新されたデータの表示
+if st.session_state.get('factor_scores_calculated', False):
+    st.success("因子得点の計算が完了しました。")
+    st.subheader("＜更新されたデータ＞")
+    st.dataframe(st.session_state.processed_data)
 
-        # 更新されたデータを表示
-        st.subheader("＜因子得点が追加されたデータ＞")
-        st.caption("因子得点の算出に使ったカラムは削除されます")
-        st.dataframe(data)
-
-        # 更新されたデータをExcelファイルとしてダウンロード
-        output = io.BytesIO()
-        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-            data.to_excel(writer, index=False, sheet_name='Updated Data')
-        
-        # ダウンロードするファイル名の設定
-        download_file_name = uploaded_file_data.name.split('.')[0] + "_因子得点算出.xlsx"
-        st.download_button(
-            label="更新されたデータをダウンロード",
-            data=output.getvalue(),
-            file_name=download_file_name,
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
+    # 更新されたデータをExcelファイルとしてダウンロード
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        st.session_state.processed_data.to_excel(writer, index=False, sheet_name='Updated Data')
+    
+    # ダウンロードするファイル名の設定
+    download_file_name = uploaded_file_data.name.split('.')[0] + "_因子得点算出.xlsx"
+    st.download_button(
+        label="更新されたデータをダウンロード",
+        data=output.getvalue(),
+        file_name=download_file_name,
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
 
 # 著作権表示
 st.markdown('© 2022-2023 Dit-Lab.(Daiki Ito). All Rights Reserved.')
